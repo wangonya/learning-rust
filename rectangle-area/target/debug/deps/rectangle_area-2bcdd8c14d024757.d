3e689e5abb69374757bb1a2d16fbe095a104ba7a
wangonya <kwangonya@gmail.com>
2020-06-19 05:43:29 +0300

    build

---
+++ rectangle-area/target/debug/deps/rectangle_area-2bcdd8c14d024757.d
@@ -0,0 +1,5 @@
+/home/wangonya/code/learning-rust/rectangle-area/target/debug/deps/rectangle_area-2bcdd8c14d024757: src/main.rs
+
+/home/wangonya/code/learning-rust/rectangle-area/target/debug/deps/rectangle_area-2bcdd8c14d024757.d: src/main.rs
+
+src/main.rs:
---
+++ rectangle-area/target/debug/rectangle-area.d
@@ -0,0 +1,1 @@
+/home/wangonya/code/learning-rust/rectangle-area/target/debug/rectangle-area: /home/wangonya/code/learning-rust/rectangle-area/src/main.rs
